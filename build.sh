4bd1ec2f3b4fbfb9af36765988e8503660d20cfa
Jesse <jesse>
2021-11-03 18:33:28 -0700

    update build

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 git clone https://github.com/plankanban/planka.git
 cd planka
 git pull
-git apply ../planka-config.patch
+
+# node:latest is not supported by planka at this time
+sed 's/FROM node AS/FROM node:lts AS/' Dockerfile
+
+cp ../server/config/env/production.js ./server/config/env/production.js
+cp ../server/config/security.js ./server/config/security.js
+
 docker build . -t tasks
